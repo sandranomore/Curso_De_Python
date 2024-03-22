20923ac06c2c48eb5fe15063670411066c4ce387
Sandra Moreno <san.morenogarcia@gmail.com>
2024-03-22 10:22:13 +0100

    nuevo

--- Curso de Python DALTO/condicionales/if-else.py
+++
@@ -1,9 +0,0 @@
-
-contraseña_almacenada = "DaltoMaestro"
-contraseña_escrita = '''DaltoMaestro'''
-
-if contraseña_almacenada == contraseña_escrita:
-    print("INICIANDO SESIÒN...")
-else: 
-    print("CONTRASEÑA EQUIVOCADA, INTENTE DE NUEVO")    
-    
--- Curso de Python DALTO/inputs/input_nombres.py
+++
@@ -1,5 +0,0 @@
-#pedirle un dato al usuario
-nombre = input("che maestro, dame tu nombre: ")
-
-#mostrando el dato
-print(f'el nombre es: {nombre}')
